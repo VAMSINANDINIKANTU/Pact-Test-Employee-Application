7259e6de7d7bdc20e07d1993323ee645bb094b42
VAMSINANDINIKANTU <56575896+VAMSINANDINIKANTU@users.noreply.github.com>
2020-06-25 15:05:25 +0530

    first commit

--- Employee Pact Application/EmployeeManageApplication/src/main/java/com/employeeapp/service/EmployeeServiceImplementation.java
+++ Employee Pact Application/EmployeeManageApplication/src/main/java/com/employeeapp/service/EmployeeServiceImplementation.java
@@ -1,5 +1,4 @@
 package com.employeeapp.service;
-
 import java.util.List;
 import java.util.Optional;
 import javax.transaction.Transactional;
@@ -46,17 +45,11 @@ public class EmployeeServiceImplementation implements EmployeeService {
 
 	@Override
 	public Employee addEmployee(Employee employee) {
-		// TODO Auto-generated method stub
 		return employeerepository.save(employee);
-				
 	}
 
 	@Override
 	public void deleteEmployee(Long id) {
-		// TODO Auto-generated method stub
 		employeerepository.deleteById(id);
-		
 	}
-
-	
 }
