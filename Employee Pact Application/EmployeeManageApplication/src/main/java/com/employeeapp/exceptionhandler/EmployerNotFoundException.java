8ed292936162c343f4c47cb247d731b1c77e3dd6
VAMSINANDINIKANTU <56575896+VAMSINANDINIKANTU@users.noreply.github.com>
2020-06-25 15:02:29 +0530

    first commit

--- Employee Pact Application/EmployeeManageApplication/src/main/java/com/employeeapp/exceptionhandler/EmployerNotFoundException.java
+++ Employee Pact Application/EmployeeManageApplication/src/main/java/com/employeeapp/exceptionhandler/EmployerNotFoundException.java
@@ -3,11 +3,10 @@ public class EmployerNotFoundException extends RuntimeException{
    private static final long serialVersionUID = 1L;
    public EmployerNotFoundException(Long employerId)
    {
-		   super("Employer id not found : " + employerId);
+       super("Employer id not found : " + employerId);
    }
    public EmployerNotFoundException(String employerName)
    {
-		   super("Employer name not found : " + employerName);
-		   
+       super("Employer name not found : " + employerName);
    }
 }
